958bced23180ac885d48b0ec01799b50fa4aa7f9
Héctor Fernández Hompanera <geralt741@gmail.com>
2022-10-14 17:15:38 +0200

    readability changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,36 +10,48 @@ public class Main {
     public static void main(final String[] args) throws InterruptedException {
         var seed = createSeed();
 
-        List<Integer> binaries = toList(seed);
-        List<Integer> random = new ArrayList<>();
+        List<String> generated = new ArrayList<>();
+        List<Byte> binaries = toList(seed);
 
-        while (true) {
-            while (random.size() <= 5) {
-                random.add(binaries.get(binaries.size() - 1));
-                binaries.remove(binaries.size() - 1);
-                Integer xor = binaries.get(binaries.size() - 1) ^ binaries.get(binaries.size() - 2);
-                binaries.add(0, xor);
-            }
-            var binary = random.stream().map(String::valueOf).collect(Collectors.joining(""));
-            var decimal = toDecimal(binary);
-            var hexadecimal = toHexadecimal(binary);
+        do {
+            List<Byte> random = generator(binaries, 24);
+            String binary = toBinaryString(random);
+            int decimal = toDecimal(binary);
+            String hexadecimal = toHexadecimal(binary).toUpperCase();
+
+            generated.add(binary);
 
             System.out.println(decimal + " - " + hexadecimal);
-            random = new ArrayList<>();
-            Thread.sleep(1000);
-        }
 
+            Thread.sleep(500);
+        } while (generated.size() == new HashSet<>(generated).size());
+
+        System.out.println("Number of pseudo-random numbers generated: " + generated.size());
 
     }
 
-    private static List<Integer> toList(String seed) {
+    private static List<Byte> generator(List<Byte> binaries, int length) {
+        List<Byte> random = new ArrayList<>();
+        while (random.size() <= length) {
+            random.add(binaries.get(binaries.size() - 1));
+            binaries.remove(binaries.size() - 1);
+            byte xor = (byte) (binaries.get(binaries.size() - 1) ^ binaries.get(binaries.size() - 2));
+            binaries.add(0, xor);
+        }
+        if (random.stream().noneMatch(b -> b != 0)) {
+            return generator(binaries, length);
+        }
+        return random;
+    }
+
+    private static List<Byte> toList(String seed) {
         return seed.chars()
                 .mapToObj(i -> (char) i)
-                .map(c -> Integer.parseInt(String.valueOf(c)))
+                .map(c -> Byte.parseByte(String.valueOf(c)))
                 .map(Integer::toBinaryString)
                 .map(s -> s.chars()
                         .mapToObj(i -> (char) i)
-                        .map(c -> Integer.parseInt(String.valueOf(c)))
+                        .map(c -> Byte.parseByte(String.valueOf(c)))
                         .collect(Collectors.toList()))
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
@@ -53,11 +66,16 @@ public class Main {
                 dateTime.getNano());
     }
 
-    public static int toDecimal(String binaryNumber) {
+    private static String toBinaryString(List<Byte> random) {
+        return random.stream().map(String::valueOf).collect(Collectors.joining(""));
+    }
+
+
+    private static int toDecimal(String binaryNumber) {
         return Integer.parseInt(binaryNumber, 2);
     }
 
-    public static String toHexadecimal(String binaryNumber) {
+    private static String toHexadecimal(String binaryNumber) {
         return Integer.toHexString(Integer.parseInt(binaryNumber, 2));
     }
 }
